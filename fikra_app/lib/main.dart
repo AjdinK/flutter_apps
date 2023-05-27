812d353f2419c0409d213c25ecf2e11f843c5caa
AjdinK <Ajdin.kuduzovic@gmail.com>
2023-05-27 15:21:48 +0200

    v1

--- fikra_app/lib/main.dart
+++ fikra_app/lib/main.dart
@@ -2,9 +2,25 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Center(
-        child: Text("this is from VS"),
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.amber[100],
+        appBar: AppBar(
+          title: const Text("Fikra"),
+          backgroundColor: Colors.grey[900],
+        ),
+        body: const Center(
+          child: Center(
+            child: Padding(
+              padding: EdgeInsets.all(30),
+              child: Image(
+                image: NetworkImage(
+                    'https://docs.flutter.dev/assets/images/flutter-logo-sharing.png'),
+              ),
+            ),
+          ),
+        ),
       ),
     ),
   );
